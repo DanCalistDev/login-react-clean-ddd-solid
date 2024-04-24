5941d110499144f6c8eac83617b3b1ca28ab70bf
DanielCalistrato <dancalist.dev@gmail.com>
2024-04-24 06:17:54 -0300

    refactor: move axios mock to a separate file

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,41 +1,37 @@
-import { HttpPostParams } from "@/data/protocols/http"
 import { AxiosHttpClient } from "./axios-http-client"
 import axios from 'axios'
-import casual from 'casual'
+import { mockAxios } from "./test"
+import { mockPostRequest } from "@/data/test"
 
 jest.mock('axios')
-const mockedAxios = axios as jest.Mocked<typeof axios>
-const mockedAxiosResult = {
-    data: casual.random_element,
-    status: casual.random
-}
-mockedAxios.post.mockResolvedValue(mockedAxiosResult)
 
-const makeSut = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
+type SutTypes = {
+  sut: AxiosHttpClient
+  mockedAxios: jest.Mocked<typeof axios>
 }
 
-const mockPostRequest = (): HttpPostParams<any> => ({
-    url: casual.url,
-    body: casual.random_element
-})
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  const mockedAxios = mockAxios()
+  return {
+    sut,
+    mockedAxios
+  }
+}
 
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
     const request = mockPostRequest()
-    const sut = makeSut()
+    const {	sut, mockedAxios} = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
     
   })
 
-  test('Should return the correct statusCode and body', async () => {
-    const sut = makeSut()
-    const httpResponse = await sut.post(mockPostRequest())
-    expect(httpResponse).toEqual({
-        statusCode: mockedAxiosResult.status,
-        body: mockedAxiosResult.data
-    })
+  test('Should return the correct statusCode and body',() => {
+    const {sut, mockedAxios} = makeSut()
+    const promise = sut.post(mockPostRequest())
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
     
   })
 
